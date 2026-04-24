b9f13846134b5014dc517ef08992ad2309b507f3
rwilliams175 <rwilliams175@phm.k12.in.us>
2026-04-24 09:46:28 -0400

    Update final_project.py

--- final_project.py
+++ final_project.py
@@ -6,21 +6,50 @@ root = tk.Tk()
 root.title("Secure Login Page")
 root.geometry("400x300")
 
-name_var = tk.StringVar()
+user_var = tk.StringVar()
+pass_var = tk.StringVar()
+newuser_var = tk.StringVar()
+newpass_var = tk.StringVar()
+verifypass_var = tk.StringVar()
 
-username_label = tk.Label(root, text="Username")
-username_entry = tk.Entry(root, textvariable = name_var)
-username_label.grid(row=0,column=0)
-username_entry.grid(row=0,column=1)
+def create_entry_field():
+    button.destroy()
+    buttontwo.destroy()
+    username_label = tk.Label(root, text="Username:")
+    username_entry = tk.Entry(root, textvariable = user_var)
+    username_label.pack()
+    username_entry.pack()
+    password_label = tk.Label(root, text="Password:")
+    password_entry = tk.Entry(root,textvariable=pass_var,show="*")
+    password_label.pack()
+    password_entry.pack()
+def create_account_field():
+    button.destroy()
+    buttontwo.destroy()
+    newuser_label = tk.Label(root, text="Create Username:")
+    newuser_entry = tk.Entry(root, textvariable = newuser_var)
+    newpass_label = tk.Label(root, text = "Create Password:")
+    newpass_entry = tk.Entry(root, textvariable = newpass_var, show = "*")
+    verifypass_label = tk.Label(root, text = "Verify Password:")
+    verifypass_entry = tk.Entry(root, textvariable = verifypass_var, show = "*")
+    newuser_label.pack()
+    newuser_entry.pack()
+    newpass_label.pack()
+    newpass_entry.pack()
+    verifypass_label.pack()
+    verifypass_entry.pack()
 
-
-
+button = tk.Button(root, text = "Login", command = create_entry_field)
+button.pack()
+buttontwo = tk.Button(root, text = "Create Account",command = create_account_field)
+buttontwo.pack()
 
 def login():
     pass
-
-#button = tk.Button(root, text = "Login", command = login)
-#button.pack()
+def create_account():
+    pass
+def forgot_password():
+    pass
 
 #runs the program above
 root.mainloop()
